55e0c3f088871f8cca2f338a40ea8c041c54458e
GoastcraftHD <simon.ickler@gmail.com>
2022-03-09 09:55:41 +0100

    Changed design

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 let upgrades = Array.from(document.querySelectorAll("#upgrades button"))
+let rickBtn = document.querySelector("#rickBtn")
+
 let upgradeValues = [1, 10, 20, 50, 100, 500]
 
 let clicks = 0
 let rps = 0
 
-document.querySelector("#rickBtn").addEventListener("click", () => {
+rickBtn.addEventListener("click", () => {
   clicks++
+
+  rickBtn.style.width = "200px"
+  rickBtn.style.height = "241px"
+  
   document.querySelector("#clickView").innerText = clicks
+
+  setTimeout(() => {
+    rickBtn.style.width = "220px"
+    rickBtn.style.height = "261px"
+  }, 100)
 })
 
 upgrades.forEach(upgrade => {
