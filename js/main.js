41d5d36b0eb6ffe1b2e9cc8c20606a2a81c8eb1e
GoastcraftHD <simon.ickler@gmail.com>
2022-03-10 11:53:19 +0100

    stfu

--- js/main.js
+++ js/main.js
@@ -2,9 +2,32 @@ let upgrades = Array.from(document.querySelectorAll("#upgrades button"))
 let rickBtn = document.querySelector("#rickBtn")
 
 let upgradeValues = [1, 10, 20, 50, 100, 500]
+let baseCost = [100, 200, 300, 400, 500, 6000]
+let upgradeAmount = [0, 0, 0, 0, 0, 0]
 
+let buyAmount = 1;
 let clicks = 0
 let rps = 0
+
+document.querySelector("body").addEventListener("keydown", event => {
+  if (event.key == "Control")
+    buyAmount = 100
+  else if (event.key == "Shift")
+    buyAmount = 10
+})
+
+document.querySelector("body").addEventListener("keyup", () => {
+  buyAmount = 1
+})
+
+upgrades.forEach(upgrade => {
+  upgrade.addEventListener("click", () => {
+    let index = upgrades.indexOf(upgrade, 0)
+    upgradeAmount[index] += buyAmount
+    document.querySelector("#upgradeDisplay" + index).innerText = upgradeAmount[index]
+    rps += upgradeAmount[index] * upgradeValues[index]
+  })
+})
 
 rickBtn.addEventListener("click", () => {
   clicks++
@@ -20,15 +43,7 @@ rickBtn.addEventListener("click", () => {
   }, 100)
 })
 
-upgrades.forEach(upgrade => {
-  upgrade.addEventListener("click", () => {
-    let index = upgrades.indexOf(upgrade)
-    rps += upgradeValues[index]
-  })
-})
-
 setInterval(() => {
-  clicks += rps / 100
+  clicks += rps / 10
   document.querySelector("#clickView").innerText = Math.round(clicks * 100) / 100
-}, 10)
-
+}, 100)
